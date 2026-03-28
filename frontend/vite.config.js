3a39736fa33b04d8db63e007c6c486944ebef142
pascaljunior26 <essononguema16@gmail.com>
2026-03-28 14:16:26 +0100

    Fix vercel deployment config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,22 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
-    strictPort: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false
-      }
-    }
-  },
   build: {
     outDir: 'dist',
-    sourcemap: false,
+    emptyOutDir: true
   },
-});
+  server: {
+    host: 'localhost',
+    port: 5173,
+    strictPort: true
+  }
+})
